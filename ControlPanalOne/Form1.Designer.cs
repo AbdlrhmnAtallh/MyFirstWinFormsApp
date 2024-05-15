e0db02474a4927cd1b79df17075ee2a648e26d59
Abdelrhmn <abdlrhmntt@gmail.com>
2024-05-15 09:17:31 +0300

    init

---
+++ ControlPanalOne/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace ControlPanalOne
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listNames = new System.Windows.Forms.ListBox();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(138, 53);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(100, 23);
+            this.buttonAdd.TabIndex = 0;
+            this.buttonAdd.Text = "Add Name";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Names";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listNames
+            // 
+            this.listNames.FormattingEnabled = true;
+            this.listNames.Location = new System.Drawing.Point(12, 27);
+            this.listNames.Name = "listNames";
+            this.listNames.Size = new System.Drawing.Size(120, 82);
+            this.listNames.TabIndex = 3;
+            this.listNames.SelectedIndexChanged += new System.EventHandler(this.listNames_SelectedIndexChanged);
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(138, 27);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(100, 20);
+            this.textName.TabIndex = 4;
+            this.textName.TextChanged += new System.EventHandler(this.textName_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(554, 401);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.listNames);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonAdd);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listNames;
+        private System.Windows.Forms.TextBox textName;
+    }
+}
+
---
+++ ControlPanalOne/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ControlPanalOne
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listNames.Items.Add("Added by default1");
+            listNames.Items.Add("Added by default2");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textName.Text) && !listNames.Items.Contains(textName.Text))
+                listNames.Items.Add(textName.Text);
+        }
+
+        private void textName_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void listNames_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
